57c622f04852c067cef8b9969f5b14c428261e1e
Roberto C <carrasquel18@gmail.com>
2019-05-28 11:53:29 -0400

    first commit

---
+++ js/blog.js
@@ -0,0 +1,28 @@
+  $(".blog").owlCarousel({
+    loop: true,
+    nav: true,
+    items: 4,
+    navText : ['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
+    margin: 10,
+    responsiveClass:true,
+    responsive:{
+    	0:{
+    		items:1,
+    		nav: true
+    	},
+    	300:{ 
+
+    		items: 3,
+    		nav: true    	
+    	},
+    	768:{
+    		items: 4,
+    		nav: true
+    	},
+    	
+    	1000:{
+    		items: 5,
+    		nav: true,
+    	}
+    }
+  });
---
+++ js/index.js
@@ -0,0 +1,50 @@
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    navText : ['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
+    margin: 10,
+    responsiveClass:true,
+    responsive:{
+    	0:{
+    		items:1,
+    		nav: true
+    	},
+    	300:{ 
+
+    		items: 3,
+    		nav: true    	
+    	},
+    	768:{
+    		items: 4,
+    		nav: true
+    	},
+    	
+    	1000:{
+    		items: 5,
+    		nav: true,
+    	}
+    }
+  });
+
+  $(window).scroll(function(){
+    var scroll = $(window).scrollTop();
+      if (scroll > 1) {
+        $(".navbar").css("background-color" , "#29a9e1");
+      }
+
+      else{
+          $(".navbar").css("background-color" , "#030819");   
+      }
+  });
+
+
+
+
+
+
+
+
+ 
+
+});
+
