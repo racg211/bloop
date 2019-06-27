4643c81e7e8870bf1fc4d21aa3ce487929275cda
Roberto C <carrasquel18@gmail.com>
2019-06-27 08:57:26 -0400

    script navbar

--- js/index.js
+++ js/index.js
@@ -56,7 +56,13 @@ window.onscroll = function() {
 }
 
 
+const links = document.querySelectorAll('.navbar-expand-lg .navbar-nav .nav-link');
 
+const navbar = document.querySelector('.navbar-collapse.collapse.show');
+
+links.forEach(link=> {
+    link.addEventListener("click", ()=> navbar.classList.remove('show'));
+});
 
 
 
